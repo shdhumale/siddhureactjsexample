1c1ec536d5e8778ab468fb32c42b3dc68e7c67c1
Siddharatha.H.Dhumale <shdhumale@gmail.com>
2020-04-03 13:37:25 +0530

    setState modification

--- my-example/src/Component/SiddhuStateButton.js
+++ my-example/src/Component/SiddhuStateButton.js
@@ -7,26 +7,48 @@ class SiddhuStateButton extends React.Component {
         super(props);
         this.state =
         {
-            message: 'This is my message'
+            message: 'This is my message',
+            count: 0
+
         }
         this.changeMessage = this.changeMessage.bind(this);
+        this.increment = this.increment.bind(this);
     }
+
 
     //note as the blow is not a function we cannot convert it into ==> function
     changeMessage() {
-        this.setState(
+
+        /* this.setState(
             {
-                message: 'Message is changed on button click'
-            }
-        )
+                message: 'Message is changed on button click',
+                count: this.state.count + 1
+            }, () => console.log('--------inside call back-------' + this.state.count)
+        ) */
+
+        this.setState((prevState) => (
+            {
+                message: 'Message is changed on button click',
+                count: prevState.count + 1
+            }//, () => console.log('--------inside call back-------' + this.state.count)
+        ))
+        console.log('-----outside call back----------' + this.state.count);
     }
 
+    increment() {
+        this.changeMessage();
+        this.changeMessage();
+        this.changeMessage();
+        this.changeMessage();
+
+    }
     render() {
         return (
             <div>
                 {/* <h1>This is my message</h1> */}
                 <h1>{this.state.message}</h1>
-                <button onClick={this.changeMessage}>Change Message</button>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increment}>Change Message</button>
             </div>
 
 
